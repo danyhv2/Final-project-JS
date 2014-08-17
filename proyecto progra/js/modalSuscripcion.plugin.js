fb94d444acdf2c34156fee5b569404518dc902ae
Edgar <edgar_hj1@hotmail.com>
2014-08-16 19:47:09 -0600

    Modal dialog del boletín informativo completo visualmente.

--- proyecto progra/js/modalSuscripcion.plugin.js
+++ proyecto progra/js/modalSuscripcion.plugin.js
@@ -22,15 +22,18 @@
 					$(this.element).click(function(){
 						if(self.settings.position == 'right'){
 
-							$(this).parent().append($('<div></div>').addClass('suscriptionDialog').addClass('col-xs-10 col-xs-offset-1 col-sm-6 col-sm-offset-3 col-lg-4 col-lg-offset-4'));
+							$(this).parent().append($('<div></div>').addClass('suscriptionDialog').addClass('col-xs-10 col-xs-offset-1 col-sm-6 col-sm-offset-3 col-lg-4 col-lg-offset-4 clearfix'));
 
 							$('.suscriptionDialog').append($('<span></span>').addClass('glyphicon glyphicon-remove-circle close'));
 
-							$('.suscriptionDialog').append($('<h1 class="txtTitulo">'+'SUSCRIPCIÓN'+'</h1>'));
-							$('.suscriptionDialog').append($('<input type="text" placeholder="Nombre" class="txtSuscripcion">').addClass('inputSuscripcion'));
-							$('.suscriptionDialog').append($('<input type="text" placeholder="Apellido" class="txtSuscripcion">').addClass('inputSuscripcion'));
-							$('.suscriptionDialog').append($('<input type="text" placeholder="Correo electrónico" class="txtSuscripcion">').addClass('inputSuscripcion'));
-							$('.suscriptionDialog').append($('<input type="checkbox">').addClass('inputSuscripcion'));
+							$('.suscriptionDialog').append($('<div>'+'<h2 class="txtTitulo">'+'NEWSLETTER'+'</h2>'+'</div>').addClass('text-center'));
+
+							$('.suscriptionDialog').append($('<input type="text" placeholder="Nombre">').addClass('txtSuscripcion').addClass('form-control input-lg'));
+							$('.suscriptionDialog').append($('<input type="text" placeholder="Apellido">').addClass('txtSuscripcion').addClass('form-control input-lg'));
+							$('.suscriptionDialog').append($('<input type="text" placeholder="Correo electrónico">').addClass('txtSuscripcion').addClass('form-control input-lg'));
+							$('.suscriptionDialog').append($('<label>'+'<input type="checkbox">'+'  Acepto recibir información por email.'+'</label>').addClass('txtAceptacion'));
+
+							$('.suscriptionDialog').append($('<button class="btnSuscripcion" type="button">'+'Suscribirme'+'</button>').addClass('btn btn-success btn-lg pull-right'));
 
 							$(this).parent().append($('<div></div>').addClass('suscriptionShadow')
 							);
